1d591745cbd09789d66045d89b6a0169137edd41
Pijus <pijus@Pijuss-MacBook-Air.local>
2021-04-11 03:45:47 +0300

    Misc. improvements.

--- projects/aimie/src/lib/aimie/buttons/aimie-button-fetching/aimie-button-fetching.component.ts
+++ projects/aimie/src/lib/aimie/buttons/aimie-button-fetching/aimie-button-fetching.component.ts
@@ -4,8 +4,8 @@ import {Component, Input} from '@angular/core';
   // tslint:disable-next-line:component-selector
   selector: 'aimie-button-fetching',
   template: `
-    <button mat-mini-fab class="sync-fab" [ngClass]="{'rotate': fetching && rotateIcon, 'spinner': fetching}">
-      <mat-icon>{{materialIcon || 'sync'}}</mat-icon>
+    <button mat-mini-fab class="sync-fab" [ngClass]="{'spinner': fetching}">
+      <mat-icon [ngClass]="{'rotate': fetching && rotateIcon}">{{materialIcon || 'sync'}}</mat-icon>
     </button>
   `,
   styleUrls: ['./aimie-button-fetching.component.scss']
